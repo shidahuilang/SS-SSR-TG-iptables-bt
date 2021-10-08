260d1d37829d6dbc44a29ed8dc8b8a95b2498f29
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-09 01:44:48 +0800

    Update mtproxy_go.sh

--- mtproxy_go.sh
+++ mtproxy_go.sh
@@ -349,8 +349,15 @@ showLog() {
 }
 
 menu() {
-    
-
+    clear
+    echo "#############################################################"
+    echo -e "#                    ${RED}MTProto一键安装脚本${PLAIN}                    #"
+    echo -e "# ${GREEN}作者${PLAIN}: 网络跳越(hijk)                                      #"
+    echo -e "# ${GREEN}网址${PLAIN}: https://hijk.art                                    #"
+    echo -e "# ${GREEN}论坛${PLAIN}: https://hijk.club                                   #"
+    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/hijkclub                               #"
+    echo -e "# ${GREEN}Youtube频道${PLAIN}: https://youtube.com/channel/UCYTB--VsObzepVJtc9yvUxQ #"
+    echo "#############################################################"
     echo ""
 
     echo -e "  ${GREEN}1.${PLAIN} 安装MTProto代理"
