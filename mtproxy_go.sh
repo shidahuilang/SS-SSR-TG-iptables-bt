61b2095cef3ac68895bbe48ad9cbfa1b954478ae
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-09 01:36:41 +0800

    Update mtproxy_go.sh

--- mtproxy_go.sh
+++ mtproxy_go.sh
@@ -8,7 +8,11 @@ YELLOW="\033[33m"   # Warning message
 BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'
 
-
+export MTG_CONFIG="${MTG_CONFIG:-$HOME/.config/mtg}"
+export MTG_ENV="$MTG_CONFIG/env"
+export MTG_SECRET="$MTG_CONFIG/secret"
+export MTG_CONTAINER="${MTG_CONTAINER:-mtg}"
+export MTG_IMAGENAME="${MTG_IMAGENAME:-nineseconds/mtg:1}"
 
 DOCKER_CMD="$(command -v docker)"
 OSNAME=`hostnamectl | grep -i system | cut -d: -f2`
@@ -346,14 +350,7 @@ showLog() {
 
 menu() {
     clear
-    echo "#############################################################"
-    echo -e "#                    ${RED}MTProto一键安装脚本${PLAIN}                    #"
-    echo -e "# ${GREEN}作者${PLAIN}: 网络跳越(hijk)                                      #"
-    echo -e "# ${GREEN}网址${PLAIN}: https://hijk.art                                    #"
-    echo -e "# ${GREEN}论坛${PLAIN}: https://hijk.club                                   #"
-    echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/hijkclub                               #"
-    echo -e "# ${GREEN}Youtube频道${PLAIN}: https://youtube.com/channel/UCYTB--VsObzepVJtc9yvUxQ #"
-    echo "#############################################################"
+
     echo ""
 
     echo -e "  ${GREEN}1.${PLAIN} 安装MTProto代理"
