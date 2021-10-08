53150d71a735d2cfe2a4a8eb225c794afc0d627d
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-09 00:15:58 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -6,8 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: MTProxy
 #	Version: 1.0.8
-#	Author: Toyo
-#	Blog: https://doub.io/shell-jc7/
+
 #=================================================
 
 sh_ver="1.0.8"
