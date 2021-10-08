797f76d5e892d7537a92f66b66777515f29b9e43
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-09 00:04:03 +0800

    Update tinymapper.sh

--- tinymapper.sh
+++ tinymapper.sh
@@ -6,8 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: tinyPortMapper
 #	Version: 1.0.2
-#	Author: Toyo
-#	Blog: https://doub.io/wlzy-36/
+
 #=================================================
 sh_ver="1.0.2"
 
@@ -371,9 +370,9 @@ View_Log(){
 	tail -f ${LOG_File}
 }
 Update_Shell(){
-	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/tinymapper.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
+	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/shidahuilang/SS-SSR-TG-iptables-bt/main/tinymapper.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/tinymapper.sh" && chmod +x tinymapper.sh
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/shidahuilang/SS-SSR-TG-iptables-bt/main/tinymapper.sh" && chmod +x tinymapper.sh
 	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 check_sys
