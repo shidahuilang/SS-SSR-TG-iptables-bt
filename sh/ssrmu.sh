bc5261b198e7c4777da95236e4ae5c73b2fde68a
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-08 19:30:09 +0800

    Update ssrmu.sh

--- sh/ssrmu.sh
+++ sh/ssrmu.sh
@@ -393,8 +393,8 @@ Set_config_port(){
 }
 Set_config_password(){
 	echo "请输入要设置的用户 密码"
-	read -e -p "(默认: doub.io):" ssr_password
-	[[ -z "${ssr_password}" ]] && ssr_password="doub.io"
+	read -e -p "(默认: langlang):" ssr_password
+	[[ -z "${ssr_password}" ]] && ssr_password="langlang"
 	echo && echo ${Separator_1} && echo -e "	密码 : ${Green_font_prefix}${ssr_password}${Font_color_suffix}" && echo ${Separator_1} && echo
 }
 Set_config_method(){
