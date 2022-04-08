39acc64f3cbcfd8ea0db3f47dcdb24b1877d8542
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-08 16:12:08 +0800

    Update pzcon.sh

--- sh/xray/pzcon.sh
+++ sh/xray/pzcon.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+qrCodeBase64Default=$(echo -n "{\"port\":${PORT},\"ps\":\"VMESS_WS_TLS_${domain}\",\"tls\":\"tls\",\"id\":\"${UUID}\",\"aid\":0,\"v\":2,\"host\":\"${domain}\",\"type\":\"none\",\"path\":\"/${VMESS_WS_PATH}/\",\"net\":\"ws\",\"add\":\"${domain}\",\"allowInsecure\":0,\"method\":\"none\",\"peer\":\"${domain}\",\"sni\":\"${domain}\"}" | base64 -w 0)
+qrCodeBase64Default="${qrCodeBase64Default// /}"
+TCPqrCodeBase64Default=$(echo -n "{\"add\":\"${domain}\",\"aid\":0,\"host\":\"${domain}\",\"id\":\"${UUID}\",\"net\":\"tcp\",\"path\":\"/${VMESS_TCP_PATH}/\",\"port\":${PORT},\"ps\":\"VMESS_TCP_TLS_${domain}\",\"scy\":\"none\",\"sni\":\"${domain}\",\"tls\":\"tls\",\"v\":2,\"type\":\"http\",\"allowInsecure\":0,\"peer\":\"${domain}\",\"obfs\":\"http\",\"obfsParam\":\"${domain}\"}" | base64)
+TCPqrCodeBase64Default="${TCPqrCodeBase64Default// /}"
 cat >/usr/local/etc/xray/pzcon <<-EOF
 #!/bin/bash
 # 此文件请勿删除和做任何修改
@@ -72,9 +77,9 @@ echo
 echo
 echo -e "\033[31m （4）VLESS+WS+TLS（可套CF使用） \033[0m"
 echo
-echo "vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain}"
+echo "vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${VLESS_WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain}"
 echo
-echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain} \033[0m"
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://${UUID}@${domain}:${PORT}?host=${domain}&path=%2F${VLESS_WS_PATH}%2F&type=ws&encryption=none&security=tls&sni=${domain}#VLESS_WS_TLS_${domain} \033[0m"
 echo
 echo -e "\033[33m 节点备注/别名：VLESS+WS+TLS${PORT}（可自行修改） \033[0m"
 echo -e "\033[33m 节点类型：XRay \033[0m"
@@ -87,10 +92,54 @@ echo -e "\033[33m TLS：勾选 \033[0m"
 echo -e "\033[33m XTLS：不选 \033[0m"
 echo -e "\033[33m alpn：默认 \033[0m"
 echo -e "\033[33m 域名：${domain} \033[0m"
-echo -e "\033[33m 传输协议：WebSocket \033[0m"
+echo -e "\033[33m 传输协议：WebSocket/ws \033[0m"
 echo -e "\033[33m WebSocket Host：${domain} \033[0m"
-echo -e "\033[33m WebSocket Path：/${WS_PATH}/ \033[0m"
+echo -e "\033[33m WebSocket Path：/${VLESS_WS_PATH}/ \033[0m"
 echo
-echo -e "\033[32m 总共4条信息查询完毕,往上翻查看  \033[0m"
 echo
+echo
+echo -e "\033[31m （5）VMESS+TCP+TLS \033[0m"
+echo
+echo "vmess://${TCPqrCodeBase64Default}"
+echo
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${TCPqrCodeBase64Default} \033[0m"
+echo
+echo -e "\033[33m 节点备注/别名：VLESS+TCP+TLS${PORT}（可自行修改） \033[0m"
+echo -e "\033[33m 节点类型：XRay \033[0m"
+echo -e "\033[33m 协议：VMESS \033[0m"
+echo -e "\033[33m 服务器地址：${domain} \033[0m"
+echo -e "\033[33m 服务器端口：${PORT} \033[0m"
+echo -e "\033[33m UUID：${UUID} \033[0m"
+echo -e "\033[33m 加密方式：AUTO \033[0m"
+echo -e "\033[33m 传输协议：TCP \033[0m"
+echo -e "\033[33m 伪装类型：HTTP \033[0m"
+echo -e "\033[33m HTTP Host：${domain} \033[0m"
+echo -e "\033[33m HTTP Path：/${VMESS_TCP_PATH}/ \033[0m"
+echo -e "\033[33m TLS：勾选 \033[0m"
+echo -e "\033[33m 指纹伪造：禁用 \033[0m"
+echo -e "\033[33m TLS Host：${domain} \033[0m"
+echo
+echo
+echo
+echo -e "\033[31m （6）VMESS+WS+TLS（可套CF使用） \033[0m"
+echo
+echo "vmess://${qrCodeBase64Default}"
+echo
+echo -e "\033[32m 二维码链接(浏览器打开):https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vmess://${qrCodeBase64Default} \033[0m"
+echo
+echo -e "\033[33m 节点备注/别名：VLESS+WS+TLS${PORT}（可自行修改） \033[0m"
+echo -e "\033[33m 节点类型：XRay \033[0m"
+echo -e "\033[33m 协议：VMESS \033[0m"
+echo -e "\033[33m 服务器地址：${domain} \033[0m"
+echo -e "\033[33m 服务器端口：${PORT} \033[0m"
+echo -e "\033[33m UUID：${UUID} \033[0m"
+echo -e "\033[33m 加密方式：AUTO \033[0m"
+echo -e "\033[33m 传输协议：WebSocket/ws \033[0m"
+echo -e "\033[33m WebSocket Path：/${VMESS_WS_PATH}/ \033[0m"
+echo -e "\033[33m TLS：勾选 \033[0m"
+echo -e "\033[33m 指纹伪造：禁用 \033[0m"
+echo -e "\033[33m TLS Host：${domain} \033[0m"
+echo
+echo
+echo -e "\033[31m 总共6条节点信息,请往上翻查看  \033[0m"
 EOF
