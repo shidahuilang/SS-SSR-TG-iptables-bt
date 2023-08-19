782d4571f60773f07fcbd2695a3ce5e048f6f7f3
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-08-19 15:06:11 +0800

    Update Subconverter.vue

--- subweb/src/views/Subconverter.vue
+++ subweb/src/views/Subconverter.vue
@@ -652,7 +652,7 @@ export default {
         clientType: "",
         customBackend: "http://127.0.0.1:25500/sub?",
         shortType: "http://127.0.0.1:25500/short",
-        remoteConfig: "https://ghproxy.com/https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini",
+        remoteConfig: "https://ghproxy.com/https://raw.githubusercontent.com/flyhigherpi/merlinclash_clash_related/master/Rule_config/ZHANG.ini",
         excludeRemarks: "",
         includeRemarks: "",
         filename: "",
