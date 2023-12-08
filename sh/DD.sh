7efc0cf7532495989572132f8b46aee79b45f6b9
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-12-08 16:00:16 +0800

    Update DD.sh

--- sh/DD.sh
+++ sh/DD.sh
@@ -173,7 +173,6 @@ easytrojan(){
 #38nps-socks5服务一键搭建脚本
 Scoks5(){
       wget -q -N --no-check-certificate https://raw.githubusercontent.com/wyx176/nps-socks5/master/install.sh && chmod 777 install.sh && bash install.sh}
-}
 #39Sing-box 全家桶
 sing-box(){
       bash <(wget -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh)
