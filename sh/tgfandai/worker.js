42534d0cac00f11ccc2039f5f86f57389bb04f03
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-12-10 15:04:06 +0800

    Create worker.js

---
+++ sh/tgfandai/worker.js
@@ -0,0 +1,26 @@
+// 地址为：IP或者域名/proxy  https://cf.dalang.workers.dev/proxy
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const actualUrlStr = url.pathname.replace("/proxy/","") + url.search + url.hash
+
+  const actualUrl = new URL(actualUrlStr)
+
+  const modifiedRequest = new Request(actualUrl, {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: 'follow'
+  });
+
+  const response = await fetch(modifiedRequest);
+  const modifiedResponse = new Response(response.body, response);
+
+  // 添加允许跨域访问的响应头
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+
+  return modifiedResponse;
+}
